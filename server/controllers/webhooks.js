79a219f139f7136baf16734eda2b61325d5f742d
AmanKumar1115 <amankr5548@gmail.com>
2025-02-05 19:11:24 +0530

    webhooks.js  update

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -26,11 +26,10 @@ export const clerkWebhooks = async (req, res) => {
                 const userData = {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
-                    name: data.first_name + data.last_name,
+                    name: data.first_name + " " + data.last_name,
                     image: data.image_url,
                     resume: ''
                 }
-
                 await User.create(userData)
                 res.json({})
                 break;
